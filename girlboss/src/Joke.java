0911992be540d4a9cfab2429e2cba31ac1c90e74
Marieta Stoycheva <mpstoycheva19@codingburgas.bg>
2023-03-08 10:43:40 +0200

    Add Joke class and Menu

---
+++ girlboss/src/Joke.java
@@ -0,0 +1,5 @@
+public class Joke {
+    String joke;
+
+
+}
--- girlboss/src/Main.java
+++ girlboss/src/Main.java
@@ -1,6 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    public static void log(String msg){
+        var tid = Thread.currentThread().getId();
+        System.out.printf("%d: %s\n", tid, msg);
+    }
+    public static void jokesMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int answer = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("____                                                  _     _                        _ _     ___ ");
+        System.out.println("|  _ \\  ___    _   _  ___  _   _  __      ____ _ _ __ | |_  | |_ ___    ___ _ __ ___ (_) | __|__ \\");
+        System.out.println("| | | |/ _ \\  | | | |/ _ \\| | | | \\ \\ /\\ / / _` | '_ \\| __| | __/ _ \\  / __| '_ ` _ \\| | |/ _ \\/ /");
+        System.out.println("| |_| | (_) | | |_| | (_) | |_| |  \\ V  V / (_| | | | | |_  | || (_) | \\__ \\ | | | | | | |  __/_| ");
+        System.out.println("|____/ \\___/   \\__, |\\___/ \\__,_|   \\_/\\_/ \\__,_|_| |_|\\__|  \\__\\___/  |___/_| |_| |_|_|_|\\___(_) ");
+        System.out.println("               |___/                                                                              ");
+
+        System.out.println("1. Feminist jokes");
+        System.out.println("2. Programmer jokes");
+        System.out.println("3. Smile :)");
+        if(answer == 1) {
+
+        } else if (answer == 2) {
+
+        } else if (answer == 3) {
+
+        } else {
+            System.out.println("Invalid answer");
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("  ____    _    ____  _     ___ ____ _   _ _____    ____    _  _____ _____ _  _______ _____ ____     ____ ___ ____  _     ____   ___  ____ ____  ");
@@ -8,6 +36,8 @@ public class Main {
         System.out.println("| |  _  / _ \\ \\___ \\| |    | | |  _| |_| | | |   | |  _  / _ \\ | | |  _| | ' /|  _| |  _| | |_) | | |  _ | || |_) | |   |  _ \\| | | \\___ \\___ \\");
         System.out.println("| |_| |/ ___ \\ ___) | |___ | | |_| |  _  | | |   | |_| |/ ___ \\| | | |___| . \\| |___| |___|  __/  | |_| || ||  _ <| |___| |_) | |_| |___) |__) |");
         System.out.println(" \\____/_/   \\_\\____/|_____|___\\____|_| |_| |_|    \\____/_/   \\_\\_| |_____|_|\\_\\_____|_____|_|      \\____|___|_| \\_\\_____|____/ \\___/|____/____/");
+
+        List<Joke> jokes = new ArrayList<>();
 
         System.out.println("Can women pee with a tampon in?");
         String answer1 = scanner.nextLine();
@@ -21,7 +51,7 @@ public class Main {
                 String answer3 = scanner.nextLine();
                 if (answer3.equals("Yes"))
                 {
-
+                    jokesMenu();
                 }
                 else {
                     System.out.println("You are not a woman!");
@@ -36,6 +66,6 @@ public class Main {
         else {
             System.out.println("You are not a woman!");
             System.exit(0);
-
+        }
     }
 }
