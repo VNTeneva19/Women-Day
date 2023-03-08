696f9f4f091d5406c778db04cb9b7fa9cef35eba
Marieta Stoycheva <mpstoycheva19@codingburgas.bg>
2023-03-08 12:32:45 +0200

    Finalise project

--- girlboss/src/Joke.java
+++ girlboss/src/Joke.java
@@ -1,44 +1,51 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Joke {
-
-    public void feministJokes(int input)
-    {
+    public void feministJokes(int input) throws InterruptedException, IOException {
         Main main = new Main();
         switch (input) {
             case 1:
                 System.out.println("What’s another name for feminism? Common sense.");
+                Thread.sleep(15000);
+                clearScreen();
                 main.jokesMenu();
                 break;
             case 2:
                 System.out.println("Instead of paying women the same as men, pay men the same as women and see how angry they get.");
+                Thread.sleep(15000);
+                clearScreen();
                 main.jokesMenu();
                 break;
             case 3:
                 System.out.println("What do you call guys who make “women in the kitchen” jokes? Single");
+                Thread.sleep(15000);
+                clearScreen();
                 main.jokesMenu();
                 break;
             case 4:
                 System.out.println("Opinions on abortions are kinda like nipples. Everyone has them, but women’s are a little bit more relevant. But all you ever see are men’s.");
+                Thread.sleep(15000);
+                clearScreen();
                 main.jokesMenu();
                 break;
             case 5:
-                System.out.println("Five Reasons You Should Help Your Wife Clean The House. ");
+                System.out.println("Five Reasons You Should Help Your Wife Clean The House: ");
                 System.out.println("1. You fucking live there too. ");
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 System.out.println("2. You fucking live there too. ");
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 System.out.println("3. You fucking live there too. ");
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -54,17 +61,26 @@ public class Joke {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+                Thread.sleep(15000);
+                clearScreen();
                 main.jokesMenu();
                 break;
             case 6:
+
                 System.out.println("When I hear the phrase “men don’t start drama”: (Pulls out every world history book ever)");
+                Thread.sleep(15000);
+                clearScreen();
                 main.jokesMenu();
                 break;
             case 7:
+
                 System.out.println("How do you know when a man is about to say something smart? When he starts his sentence with, “A woman once told me…”");
+                Thread.sleep(15000);
+                clearScreen();
                 main.jokesMenu();
                 break;
             default:
+
                 System.out.println("Wrong input!");
                 System.out.println("Choose a number from 1 - 7");
                 Scanner scanner = new Scanner(System.in);
@@ -74,65 +90,51 @@ public class Joke {
         }
     }
 
-    public void programmerJokes(int input) {
+    public void programmerJokes(int input) throws InterruptedException, IOException {
         Main programmerMain = new Main();
         switch (input) {
             case 1:
-                System.out.println("What’s another name for feminism? Common sense.");
+
+                System.out.println("Why did the two Java methods get a divorce? Because they had constant arguments.");
+                Thread.sleep(15000);
+                clearScreen();
                 programmerMain.jokesMenu();
                 break;
             case 2:
-                System.out.println("Instead of paying women the same as men, pay men the same as women and see how angry they get.");
+
+                System.out.println("Why did the private classes break up? Because they never saw each other.");
+                Thread.sleep(15000);
+                clearScreen();
                 programmerMain.jokesMenu();
                 break;
             case 3:
-                System.out.println("What do you call guys who make “women in the kitchen” jokes? Single");
+                System.out.println("Why did the Java developer teach his young kids about single quotes? Because they build character. ");
+                Thread.sleep(15000);
+                clearScreen();
                 programmerMain.jokesMenu();
                 break;
             case 4:
-                System.out.println("Opinions on abortions are kinda like nipples. Everyone has them, but women’s are a little bit more relevant. But all you ever see are men’s.");
+                System.out.println("Why do programmers confuse Christmas with Halloween? Because OCT 31 is the same as DEC 25.");
+                Thread.sleep(15000);
+                clearScreen();
                 programmerMain.jokesMenu();
                 break;
             case 5:
-                System.out.println("Five Reasons You Should Help Your Wife Clean The House. ");
-                System.out.println("1. You fucking live there too. ");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("2. You fucking live there too. ");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("3. You fucking live there too. ");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("4. You fucking live there too. ");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("5. You fucking live there too. ");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+                System.out.println("Why did the Java developer quit his job? Because he didn't get arrays.");
+                Thread.sleep(15000);
+                clearScreen();
                 programmerMain.jokesMenu();
                 break;
             case 6:
-                System.out.println("When I hear the phrase “men don’t start drama”: (Pulls out every world history book ever)");
+                System.out.println("Java is the most misogynistic programming language in existence. It thinks women are objects.");
+                Thread.sleep(15000);
+                clearScreen();
                 programmerMain.jokesMenu();
                 break;
             case 7:
-                System.out.println("How do you know when a man is about to say something smart? When he starts his sentence with, “A woman once told me…”");
+                System.out.println("Why does C get all the girls, and Java gets none? Because C doesn't treat them as objects.");
+                Thread.sleep(15000);
+                clearScreen();
                 programmerMain.jokesMenu();
                 break;
             default:
@@ -140,9 +142,16 @@ public class Joke {
                 System.out.println("Choose a number from 1 - 7");
                 Scanner scanner = new Scanner(System.in);
                 int answer = Integer.parseInt(scanner.nextLine());
-                feministJokes(answer);
+                programmerJokes(answer);
                 break;
         }
+    }
+    public void clearScreen() {
+        System.out.println("                                 ");
+        System.out.println("                                 ");
+        System.out.println("                                 ");
+        System.out.println("                                 ");
+        System.out.println("                                 ");
     }
 
 }
--- girlboss/src/Main.java
+++ girlboss/src/Main.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void mainMenu() {
+    public static void mainMenu() throws InterruptedException, IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("  ____    _    ____  _     ___ ____ _   _ _____    ____    _  _____ _____ _  _______ _____ ____     ____ ___ ____  _     ____   ___  ____ ____  ");
         System.out.println(" / ___|  / \\  / ___|| |   |_ _/ ___| | | |_   _|  / ___|  / \\|_   _| ____| |/ / ____| ____|  _ \\   / ___|_ _|  _ \\| |   | __ ) / _ \\/ ___/ ___|");
@@ -39,11 +40,11 @@ public class Main {
         }
     }
 
-    public static void jokesMenu() {
+    public static void jokesMenu() throws InterruptedException, IOException {
         Scanner scanner = new Scanner(System.in);
         Joke joke = new Joke();
 
-
+        joke.clearScreen();
         System.out.println("____                                                  _     _                        _ _     ___ ");
         System.out.println("|  _ \\  ___    _   _  ___  _   _  __      ____ _ _ __ | |_  | |_ ___    ___ _ __ ___ (_) | __|__ \\");
         System.out.println("| | | |/ _ \\  | | | |/ _ \\| | | | \\ \\ /\\ / / _` | '_ \\| __| | __/ _ \\  / __| '_ ` _ \\| | |/ _ \\/ /");
@@ -54,22 +55,45 @@ public class Main {
         System.out.println("1. Feminist jokes");
         System.out.println("2. Programmer jokes");
         System.out.println("3. Smile :)");
+        System.out.println("0. Quit ;)");
         int answer = Integer.parseInt(scanner.nextLine());
 
         if(answer == 1) {
+            joke.clearScreen();
+            joke.clearScreen();
+            joke.clearScreen();
             System.out.println("Choose a number from 1 - 7");
             int feministAnswer = Integer.parseInt(scanner.nextLine());
             joke.feministJokes(feministAnswer);
+        } else if (answer == 2) {
+            joke.clearScreen();
+            joke.clearScreen();
+            joke.clearScreen();
+            System.out.println("Choose a number from 1 - 7");
+            int programmerAnswer = Integer.parseInt(scanner.nextLine());
+            joke.programmerJokes(programmerAnswer);
+        } else if (answer == 3) {
+            System.out.println("     .-\"\"\"\"\"\"-.");
+            System.out.println("   .'          '.");
+            System.out.println("  /   O      O   \\");
+            System.out.println(" :                :");
+            System.out.println(" |                | ");
+            System.out.println(" : ',          ,' :");
+            System.out.println("  \\  '-......-'  /");
+            System.out.println("   '.          .'");
+            System.out.println("     '-......-'");
+
+            Thread.sleep(15000);
+            jokesMenu();
+        } else if (answer == 0) {
+            System.out.println("Smile more...");
+            Thread.sleep(1000);
+            System.exit(0);
+        } else {
+            System.out.println("Invalid answer");
         }
-//        else if (answer == 2) {
-//
-//        } else if (answer == 3) {
-//
-//        } else {
-//            System.out.println("Invalid answer");
-//        }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, IOException {
         mainMenu();
     }
 }
